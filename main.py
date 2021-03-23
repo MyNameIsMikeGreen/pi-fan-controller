90cccdb7ce75354b719f35b254890484f271049e
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-03-23 22:53:20 +0000

    Use logging rather than prints

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import logging
 import subprocess
 import time
 
 from gpiozero import OutputDevice
 
-DEBUG = False
+LOG_LEVEL = logging.INFO
 
 ON_THRESHOLD_CELSIUS = 65   # Temperature at which to start the fan
 OFF_THRESHOLD_CELSIUS = 50  # Temperature at which to stop the fan
@@ -35,7 +36,8 @@ def main():
     Turns fan on if temperature passes upper threshold.
     Turns fan off if temperature passes lower threshold.
     """
-    if DEBUG: print("Fan controller started.")
+    logger = create_logger()
+    logger.info("Fan controller started.")
     if OFF_THRESHOLD_CELSIUS >= ON_THRESHOLD_CELSIUS:
         raise RuntimeError('OFF_THRESHOLD_CELSIUS must be less than ON_THRESHOLD_CELSIUS')
 
@@ -43,15 +45,15 @@ def main():
 
     while True:
         temperature = get_temperature()
-        if DEBUG: print(f"Measured temperature: {temperature}C")
+        logger.debug(f"Measured temperature: {temperature}C")
         if temperature >= ON_THRESHOLD_CELSIUS:
             if fan.is_off():
-                if DEBUG: print(f"Temperature has risen above threshold of {ON_THRESHOLD_CELSIUS}, turning fan on...")
+                logger.info(f"Temperature has risen above threshold of {ON_THRESHOLD_CELSIUS}, turning fan on...")
                 fan.turn_on()
 
         elif temperature <= OFF_THRESHOLD_CELSIUS:
             if fan.is_on():
-                if DEBUG: print(f"Temperature has fallen below threshold of {OFF_THRESHOLD_CELSIUS}, turning fan off...")
+                logger.info(f"Temperature has fallen below threshold of {OFF_THRESHOLD_CELSIUS}, turning fan off...")
                 fan.turn_off()
 
         time.sleep(SLEEP_TIME_SECONDS)
@@ -70,6 +72,17 @@ def get_temperature():
         return float(output.split('=')[1].split('\'')[0])
     except (IndexError, ValueError):
         raise RuntimeError('Could not parse temperature output.')
+
+
+def create_logger():
+    logger = logging.getLogger('pi_fan_controller')
+    logger.setLevel(LOG_LEVEL)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
 
 
 if __name__ == '__main__':
