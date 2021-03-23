1d58425d4ed0ed2bf5759bc39e0cdede67b87bd9
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-03-23 23:00:21 +0000

    Simplify logging

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ def main():
     Turns fan on if temperature passes upper threshold.
     Turns fan off if temperature passes lower threshold.
     """
-    logger = create_logger()
-    logger.info("Fan controller started.")
+    logging.basicConfig(level=LOG_LEVEL, format="%(asctime)s | %(name)s | %(levelname)s | %(message)s")
+    logging.info("Fan controller started.")
     if OFF_THRESHOLD_CELSIUS >= ON_THRESHOLD_CELSIUS:
         raise RuntimeError('OFF_THRESHOLD_CELSIUS must be less than ON_THRESHOLD_CELSIUS')
 
@@ -45,15 +45,15 @@ def main():
 
     while True:
         temperature = get_temperature()
-        logger.debug(f"Measured temperature: {temperature}C")
+        logging.debug(f"Measured temperature: {temperature}C")
         if temperature >= ON_THRESHOLD_CELSIUS:
             if fan.is_off():
-                logger.info(f"Temperature has risen above threshold of {ON_THRESHOLD_CELSIUS}, turning fan on...")
+                logging.info(f"Temperature has risen above threshold of {ON_THRESHOLD_CELSIUS}, turning fan on...")
                 fan.turn_on()
 
         elif temperature <= OFF_THRESHOLD_CELSIUS:
             if fan.is_on():
-                logger.info(f"Temperature has fallen below threshold of {OFF_THRESHOLD_CELSIUS}, turning fan off...")
+                logging.info(f"Temperature has fallen below threshold of {OFF_THRESHOLD_CELSIUS}, turning fan off...")
                 fan.turn_off()
 
         time.sleep(SLEEP_TIME_SECONDS)
@@ -72,17 +72,6 @@ def get_temperature():
         return float(output.split('=')[1].split('\'')[0])
     except (IndexError, ValueError):
         raise RuntimeError('Could not parse temperature output.')
-
-
-def create_logger():
-    logger = logging.getLogger('pi_fan_controller')
-    logger.setLevel(LOG_LEVEL)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    return logger
 
 
 if __name__ == '__main__':
